756b67197e2f1039439b2f2c82ab8772d789a9e2
chelo7717 <chelo336@gmail.com>
2023-03-20 21:55:53 -0400

    me muestra V para el numero 5

--- src/romanos.js
+++ src/romanos.js
@@ -3,15 +3,22 @@ function convertirRomanos(num) {
   var romano="";
   while(num>0){
     cant=parseInt(num/10);
-    if(num == 4){
-      romano += "IV"
-      num -= 4
-    }else{
-      for(var i=0;i<num;i++){
-        romano+="I"
-      }
-      num = 0;
+    if(num>=5){
+      romano += "V"
+      num-=5
     }
+    else{
+      if(num == 4){
+        romano += "IV"
+        num -= 4
+      }else{
+        for(var i=0;i<num;i++){
+          romano+="I"
+        }
+        num = 0;
+      }
+    }
+    
     
   }
 
