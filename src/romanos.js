b51738c6694443560ea655fe923f7f4329e57b13
chelo7717 <chelo336@gmail.com>
2023-03-20 22:05:44 -0400

    me muestra IX para el numero 9

--- src/romanos.js
+++ src/romanos.js
@@ -3,23 +3,26 @@ function convertirRomanos(num) {
   var romano="";
   while(num>0){
     cant=parseInt(num/10);
-    if(num>=5){
-      romano += "V"
-      num-=5
-    }
-    else{
-      if(num == 4){
-        romano += "IV"
-        num -= 4
-      }else{
-        for(var i=0;i<num;i++){
-          romano+="I"
-        }
-        num = 0;
+    if(num == 9){
+      romano += "IX"
+      num -= 9
+    }else{
+      if(num >= 5){
+        romano += "V"
+        num -= 5
       }
-    }
-    
-    
+      else{
+        if(num == 4){
+          romano += "IV"
+          num -= 4
+        }else{
+          for(var i = 0;i < num;i++){
+            romano += "I"
+          }
+          num = 0;
+        }
+      } 
+    }   
   }
 
   return romano;
